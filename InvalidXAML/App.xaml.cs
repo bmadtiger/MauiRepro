5c706546fe03dd936ad94a1672f9ae47de79f381
bmadtiger <bmadtiger@outlook.com>
2024-10-29 20:21:35 +1030

    InvalidXML Project created from template

---
+++ InvalidXAML/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace InvalidXAML;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ InvalidXAML/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace InvalidXAML;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
