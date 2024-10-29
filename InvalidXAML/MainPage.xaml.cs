db35ba36b24a4c107260246a79337f26f47f354c
bmadtiger <bmadtiger@outlook.com>
2024-10-29 20:42:47 +1030

    Add some binding

--- InvalidXAML/MainPage.xaml.cs
+++ InvalidXAML/MainPage.xaml.cs
@@ -4,9 +4,13 @@ public partial class MainPage : ContentPage
 {
     int count = 0;
 
+    public string Headline { get; private set; }
+
     public MainPage()
     {
         InitializeComponent();
+        Headline = "Hello, World!";
+        BindingContext = this;
     }
 
     private void OnCounterClicked(object sender, EventArgs e)
